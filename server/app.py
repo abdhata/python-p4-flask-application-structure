862c790147e46220c655dcd06de245086d0d74c8
abdhata <abdhata@outlook.com>
2023-09-14 00:28:35 +0300

    Built my first Flask web application

--- server/app.py
+++ server/app.py
@@ -3,3 +3,19 @@
 from flask import Flask
 
 app = Flask(__name__)
+# append to app/app.py
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my app!</h1>'
+
+# modify user() in app/app.py
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+# append to app/app.py
+
+if __name__ == '__main__':
+    app.run(port=5555)
